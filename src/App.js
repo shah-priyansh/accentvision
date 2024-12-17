05976ad0236edf55aee7a2f6e9075b28bed374db
Priyansh shah <89834223+shah-priyansh@users.noreply.github.com>
2024-12-17 10:15:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // import Home from "./pages/Home";
